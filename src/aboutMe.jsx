d44f99ad3a552d0017112e79c3ddfef3facff1fa
JorgePG-L <jorgepiqueras92@gmail.com>
2024-02-06 23:36:37 +0100

    about me y servicios

--- src/aboutMe.jsx
+++ src/aboutMe.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import image_01 from '../public/Jorge_Pose.png'
+import './App.css'
 
-
-
-function aboutMe() {
+function AboutMe() {
     return ( 
-
-    <about className='AboutMe' >
-        <div className="name">
-            <h1>Jorge Piqueras</h1>
+        <div className='AboutMe'>
+            <div className="content">
+                <div className="left">
+                    <h1>Jorge Piqueras</h1>
+                </div>
+                <div className="center">
+                    <img src={image_01} alt="Jorge_Pose" className="image" />
+                </div>
+                <div className="right">
+                    <p>Texto a la derecha</p>
+                </div>
+            </div>
         </div>
-        
-
-    </about> );
+    );
 }
 
-export default aboutMe;
+export default AboutMe;
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Header from './header.jsx'
-
+import AboutMe from './aboutMe.jsx'
+import Skills from './skills.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
+    <AboutMe />
+    <Skills />
     
   </React.StrictMode>,
 )
